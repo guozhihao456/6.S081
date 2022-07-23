c17283b4b491de2728e7b1dfaceee8880d6d9977
guozhihao456 <gzh48692020@163.com>
2022-07-23 15:20:40 +0800

    nouse

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -131,9 +131,9 @@ static uint64 (*syscalls[])(void) = {
 [SYS_trace]   sys_trace,
 };
 
-static char* syscall_name[] ={"fork","exit","wait","pipe","read","kill","exec","fstat",
-                              "chdir" , "dup" ,"getpid","sbrk","sleep","uptime","open",
-                              "write","mknod","unlink","link","mkdir","close","trace"};
+//static char* syscall_name[] ={"fork","exit","wait","pipe","read","kill","exec","fstat",
+//                              "chdir" , "dup" ,"getpid","sbrk","sleep","uptime","open",
+//                              "write","mknod","unlink","link","mkdir","close","trace"};
 
 void
 syscall(void)
@@ -144,7 +144,8 @@ syscall(void)
   num = p->trapframe->a7;
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
       //a0 : 系统调用后的返回值
-    p->trapframe->a0 = syscalls[num]();
+      p->trapframe->a0 = syscalls[num]();
+
   } else {
     printf("%d %s: unknown sys call %d\n",
             p->pid, p->name, num);
